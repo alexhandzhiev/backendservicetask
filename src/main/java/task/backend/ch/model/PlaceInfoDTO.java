f587ca10b8711dc9b2283db720574aef653a09fe
Alex Handzhiev <alex.handzhiev@gmail.com>
2024-06-16 14:20:02 +0300

    dto styling

--- src/main/java/task/backend/ch/model/PlaceInfoDTO.java
+++ src/main/java/task/backend/ch/model/PlaceInfoDTO.java
@@ -9,16 +9,19 @@ import java.util.List;
 public class PlaceInfoDTO {
     private String name;
     private String address;
-     private PlaceInfo.OpeningDays openingDays;
-        public static class OpeningDays {
-            private LinkedHashMap<String, List<task.backend.ch.model.PlaceInfo.OpeningDays.OpeningTime>> days;
-            public OpeningDays(LinkedHashMap<String, List<task.backend.ch.model.PlaceInfo.OpeningDays.OpeningTime>> openingDays) {
-                this.days = openingDays;
-            }
-            public static class OpeningTime {
-                private String start;
-                private String end;
-                private String type;
+    private PlaceInfo.OpeningDays openingDays;
+
+    public static class OpeningDays {
+        private LinkedHashMap<String, List<task.backend.ch.model.PlaceInfo.OpeningDays.OpeningTime>> days;
+
+        public OpeningDays(LinkedHashMap<String, List<task.backend.ch.model.PlaceInfo.OpeningDays.OpeningTime>> openingDays) {
+            this.days = openingDays;
+        }
+
+        public static class OpeningTime {
+            private String start;
+            private String end;
+            private String type;
         }
     }
 }
